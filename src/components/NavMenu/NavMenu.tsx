318f2a84b886977ebbd5233f4999cd0665423b69
bektemirovkam <55695037+bektemirovkam@users.noreply.github.com>
2022-04-29 23:19:34 +0600

    card btn textfield

---
+++ src/components/NavMenu/NavMenu.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+
+import styles from "./Select.module.scss";
+import { NavMenuProps } from "./NavMenu.props";
+
+const NavMenu: FC<NavMenuProps> = () => {
+  return <div className="navmenu">Select</div>;
+};
+
+export default NavMenu;
