821ca92607500c8376e76635a4ef0d3e9231514b
bektemirovkam <55695037+bektemirovkam@users.noreply.github.com>
2022-04-29 23:51:21 +0600

    navmenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, useState } from "react";
-import { Button, Card, TextField } from "./components";
+import { Button, Card, NavMenu, TextField } from "./components";
 
 function App() {
   const [value, setValue] = useState("");
@@ -11,11 +11,7 @@ function App() {
   return (
     <div className="app">
       <div className="container">
-        <Card
-          count={5555444}
-          title="Make video about F55"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        />
+        <NavMenu />
       </div>
     </div>
   );
--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -1,10 +1,22 @@
 import React, { FC } from "react";
+import cn from "classnames";
 
-import styles from "./Select.module.scss";
+import styles from "./NavMenu.module.scss";
+import { ReactComponent as ArrowIcon } from "./icons/arrow.svg";
+import { ReactComponent as UserIcon } from "./icons/user_icon.svg";
 import { NavMenuProps } from "./NavMenu.props";
+import Button from "../Button/Button";
 
 const NavMenu: FC<NavMenuProps> = () => {
-  return <div className="navmenu">Select</div>;
+  return (
+    <div className={cn(styles.menu, "navmenu")}>
+      <Button className={styles.toggler} appearance="darkBlue">
+        <UserIcon />
+        <span>Василий Пупкин</span>
+        <ArrowIcon />
+      </Button>
+    </div>
+  );
 };
 
 export default NavMenu;
